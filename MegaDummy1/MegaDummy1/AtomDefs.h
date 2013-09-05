01b7103477d77e32647ee90388304e65893bd2a7
johnvanlaanen <johnvanlaanen@gmail.com>
2013-09-05 10:14:26 -0600

    Add MegaDummy1

---
+++ MegaDummy1/MegaDummy1/AtomDefs.h
@@ -0,0 +1,39 @@
+/*
+ * AtomDefs.h
+ *
+ * Created: 9/2/2013 12:07:32 PM
+ *  Author: John
+ */ 
+
+
+#ifndef ATOMDEFS_H_
+#define ATOMDEFS_H_
+
+
+#define		kPORTA		0x0800
+#define		kPORTB		0x1000
+#define		kPORTC		0x0200
+#define		kPORTD		0x0400
+
+#define		INPUT 0x0
+#define		OUTPUT 0x1
+#define		INPUT_PULLUP 0x2
+
+#define HIGH 0x1
+#define LOW  0x0
+
+#define true 0x1
+#define false 0x0
+
+#define		kBIT0		0x01
+#define		kBIT1		0x02
+#define		kBIT2		0x04
+#define		kBIT3		0x08
+#define		kBIT4		0x10
+#define		kBIT5		0x20
+#define		kBIT6		0x40
+#define		kBIT7		0x80
+
+
+
+#endif /* ATOMDEFS_H_ */
---
+++ MegaDummy1/MegaDummy1/MegaDummy1.h
@@ -0,0 +1,39 @@
+/*
+ * MegaDummy1.h
+ *
+ * Created: 9/5/2013 8:30:18 AM
+ *  Author: John
+ */ 
+
+
+#ifndef MEGADUMMY1_H_
+#define MEGADUMMY1_H_
+
+
+
+#define  kPIN_IODATA2					(kPORTB | kBIT0 )
+#define  kPIN_IODATA1					(kPORTB | kBIT1 )
+#define  kPIN_LEDSPI_SS_L				(kPORTB | kBIT2 )
+#define  kPIN_LEDSPI_MOSI				(kPORTB | kBIT3 )
+#define  kPIN_ENABLE_BUS_POWER			(kPORTB | kBIT4 )
+#define  kPIN_LEDSPI_SCK				(kPORTB | kBIT5 )
+
+#define  kPIN_BATTERY_V_SENSE			(kPORTC | kBIT0 )
+#define  kPIN_REQ_SLEEP_MODE			(kPORTC | kBIT1 )
+#define  kPIN_CHARGER_ENABLE			(kPORTC | kBIT2 )
+#define  kPIN_CHARGER_SEL_HIGH_CURRENT	(kPORTC | kBIT3 )
+#define  kPIN_IODATA3					(kPORTC | kBIT4 )
+#define  kPIN_IODATA4					(kPORTC | kBIT5 )
+
+#define  kPIN_BT_RX						(kPORTD | kBIT0 )
+#define  kPIN_BT_TX						(kPORTD | kBIT1 )
+#define  kPIN_BT_CTS_L					(kPORTD | kBIT2 )
+#define  kPIN_BT_RTS_L					(kPORTD | kBIT3 )
+#define  kPIN_IODATA8					(kPORTD | kBIT4 )
+#define  kPIN_IODATA7					(kPORTD | kBIT5 )
+#define  kPIN_IODATA6					(kPORTD | kBIT6 )
+#define  kPIN_IODATA5					(kPORTD | kBIT7 )
+
+
+
+#endif /* MEGADUMMY1_H_ */
