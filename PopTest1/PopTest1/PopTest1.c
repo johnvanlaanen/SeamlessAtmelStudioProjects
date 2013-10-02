de1a94c0d2ed892fdd0117242de74e4a88b232b8
johnvanlaanen <johnvanlaanen@gmail.com>
2013-10-02 16:36:34 -0600

    Work in progress - change to using an ISR and Timer1

--- PopTest1/PopTest1/PopTest1.c
+++ PopTest1/PopTest1/PopTest1.c
@@ -10,10 +10,28 @@
 #include "moduleIO.h"
 #include "PopTest1.h"
 
+#ifdef USE_TIMER1_ISR
+/****************************
+The timer 1 interrupt service routine
+SImply Toggle the output port
+****************************/
+ISR(TIM1_COMPA_vect)
+{
+	static uint8_t control_state=0;
+
+	// Startup delay - lets voltages settle
+	if(control_state == 0) {
+		control_state = 1;
+		digitalWrite(kPIN_BOOST_CTRL1, HIGH);
+		} else {
+		control_state = 0;
+		digitalWrite(kPIN_BOOST_CTRL1, LOW);
+	}
+}
+#endif
+
 int main(void)
 {
-	
-	
 	// set up the i/o ports: set the output values
 	digitalWrite(kPIN_ACTUATE,			LOW);
 	digitalWrite(kPIN_TRIPLER_SWITCH3,	LOW);
@@ -27,13 +45,22 @@ int main(void)
 	pinMode(kPIN_TRIPLER_SWITCH1,	INPUT);
 	pinMode(kPIN_TRIPLER_SWITCH2,	INPUT);
 	pinMode(kPIN_TRIPLER_SWITCH3,	OUTPUT);
-		
+
+#ifdef USE_TIMER1_ISR
+	// set up Timer 1 to generate a periodic interrupt for toggling the voltage booster switch
+	OCR1AL = kBOOST_TIMER_COUNT_VALUE;								// sets the A compare value for clock generation
+	TIMSK1 = (1 << OCIE1A);											// enable interrupt on compare A match
+	TCCR1B = (1 << WGM12) || (kBOOST_TIMER_PRESCALE_SELECT<<CS10);	// set timer CTC mode and prescaler divider ratio (turns on timer)
+	
+	sei();	// enable interrupts
+#else		
 	// set up 8-bit timer 0 to generate a clock output on kPIN_TRIPLER_SWITCH
 	TCCR0A = (1<<COM0A0) | (1<<WGM01);	// Toggle OC0A on match, and CTC mode
 	OCR0A = kTCCR0_COUNT_VALUE;				// sets the A compare value for clock generation
 	TCNT0 = 0;								// make sure the count starts at 0 so no short first cycle
 	GTCCR = (1<<PSR10);						// reset the prescaler
 	TCCR0B = (kTCCR0_PRESCALE_SELECT<<CS00);	// set timer prescaler divider (turns on timer)
+#endif
 
 	// set up the ADC
 	ADCSRA = (1<<ADEN) | (7<<ADPS0); // Set the ADC clock prescaler to divide by 128, and enable the ADC
--- PopTest1/PopTest1/PopTest1.h
+++ PopTest1/PopTest1/PopTest1.h
@@ -9,29 +9,43 @@
 #ifndef POPTEST1_H_
 #define POPTEST1_H_
 
+#define USE_TIMER1_ISR
 
-#define  kPIN_CAP_V_SENSE			   (kPORTA | kBIT0 )
-#define  kPIN_ACTUATE				   (kPORTA | kBIT2 )
-#define  kPIN_IODATA				   (kPORTA | kBIT3 )
-#define  kPIN_LED_ON_L				   (kPORTA | kBIT7 )
 
-#define  kPIN_TRIPLER_SWITCH1  (kPORTB | kBIT0 )
-#define  kPIN_TRIPLER_SWITCH2  (kPORTB | kBIT1 )
-#define  kPIN_TRIPLER_SWITCH3  (kPORTB | kBIT2 )
+#define  kPIN_CAP_V_SENSE		(kPORTA | kBIT0 )
+#define  kPIN_ACTUATE			(kPORTA | kBIT2 )
+#define  kPIN_IODATA			(kPORTA | kBIT3 )
+#define  kPIN_LED_ON_L			(kPORTA | kBIT7 )
 
+#define  kPIN_TRIPLER_SWITCH1	(kPORTB | kBIT0 )
+#define  kPIN_TRIPLER_SWITCH2	(kPORTB | kBIT1 )
+#define  kPIN_TRIPLER_SWITCH3	(kPORTB | kBIT2 )
 
 #define kCPU_CLOCK_FREQ_KHz 8000				// System clock frequency, in kHz
-#define kTRIPLER_FREQ_HZ 2000					// desired clock rate, in Hz
-const uint8_t  kTCCR0_PRESCALE_SELECT = 3;		// timer0 prescaler divider: prescaler out is i/o clock divided by 64
+#define kTRIPLER_FREQ_HZ 3000					// desired clock rate, in Hz
 
-// have to calculate the following value outside of the compiler to get good accuracy (16-bit math limitations)
-// count = (kCPU_CLOCK_FREQ_KHz * 1000 /64)  / (2*kTRIPLER_FREQ_HZ) )-1)
-//const uint8_t  kTCCR0_COUNT_VALUE = 61;		// yields a clock freq of 2016Hz, or an output frequency of 1008Hz
-//const uint8_t  kTCCR0_COUNT_VALUE = 30;		// yields a clock freq of 4032Hz, or an output frequency of 2016Hz
-const uint8_t  kTCCR0_COUNT_VALUE = 20 ;		// yields a clock freq of 5952Hz, or an output frequency of 2976Hz
-//const uint8_t  kTCCR0_COUNT_VALUE = 14 ;		// yields a clock freq of 8333Hz, or an output frequency of 4167Hz
-//const uint8_t  kTCCR0_COUNT_VALUE = 11 ;		// yields a clock freq of 1-417Hz, or an output frequency of 5208Hz
+#ifdef USE_TIMER1_ISR
+	#define kBOOST_TIMER_PRESCALE_RATIO 64				// the timer clock prescale divider ratio
+	#define kBOOST_TIMER_PRESCALE_SELECT  3				// timer prescal divider selection to corresponding to the above ratio
+	// caution: calculation structured carefully to not overflow the 16-bit math of the compiler
+	// note that the timer runs at twice the desired frequency since the output toggles with every full count
+	#define kPreCountDenom  (kTRIPLER_FREQ_HZ/5)
+	#define  kBOOST_TIMER_COUNT_VALUE  ( ( ( (100 * (kCPU_CLOCK_FREQ_KHz / 64)) + (kPreCountDenom/2) ) / kPreCountDenom ) - 1)
+#else
+	#define kkTCCR0_PRESCALE_RATIO 64				// the timer clock prescale divider ratio
+	const uint8_t  kTCCR0_PRESCALE_SELECT = 3;		// timer0 prescal divider selection to correspond to the above ratio
+	// caution: calculations structured carefully to not overflow 16-bit math
+	// note that the timer runs at twice the desired frequency since the output toggles with every full count
+	#define kPreCountDenom  (kTRIPLER_FREQ_HZ/5)
+	const uint8_t  kTCCR0_COUNT_VALUE = ( ( (100 * (kCPU_CLOCK_FREQ_KHz / 64)) + (kPreCountDenom/2) ) / kPreCountDenom ) - 1;
+#endif
 
-const int16_t kFullChargeADCValue = 938;	// corresponds to 11V/4 with a 3V supply
+#define kSupplyVoltage_x100		270				// 2.7V supply, used as ADC reference, times 100
+#define kADCBits				10				// the number of bits in the ADC result
+#define kFullChargeVolts_x100	900				// full charge boosted voltage, times 100
+// caution: calculations structured carefully to not overflow 16-bit math
+//const int16_t kFullChargeADCValue = ( (kFullChargeVolts_x100 / 4) * (1<<(kADCBits-3)) / (kSupplyVoltage_x100 / (1<<3)) );
+#define kFullChargeADCValue ( (kFullChargeVolts_x100 / 4) * (1<<(kADCBits-kADC_Calc_shift_bits)) / (kSupplyVoltage_x100 / (1<<kADC_Calc_shift_bits)) )
+
 
 #endif /* PWRMGR_TEST1_H_ */
